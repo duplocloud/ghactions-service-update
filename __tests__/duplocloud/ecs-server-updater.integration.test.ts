220a77344f07e67d73ef8e4a631a84a53eda4c3d
Joe Khoobyar <joe@duplocloud.net>
2022-03-03 18:49:00 -0500

    reorganize tests as integration tests

---
+++ __tests__/duplocloud/ecs-server-updater.integration.test.ts
@@ -0,0 +1,65 @@
+import {expect, jest} from '@jest/globals'
+import {DataSource} from '../../src/duplocloud/datasource'
+import {DuploHttpClient} from '../../src/duplocloud/httpclient'
+import {EcsTaskDefinitionArn} from '../../src/duplocloud/model'
+import {EcsServiceUpdater} from '../../src/ecs-service-updater'
+
+describe('EcsServiceUpdater integration', () => {
+  // Integration tests.
+  const tenantId = process.env.duplo_tenant_id
+  if (!tenantId || !process.env.duplo_token) {
+    console.log('Skipping integration tests: duplo_token and/or duplo_tenant_id env var missing or empty')
+    it('is skipped', () => {})
+  } else {
+    describe('buildServiceUpdate', () => {
+      jest.setTimeout(15000)
+
+      it('patches an ECS service', async () => {
+        const ds = new DataSource(new DuploHttpClient())
+
+        // Get tenant
+        const tenant = await ds.getTenant(tenantId).toPromise()
+        expect(tenant).not.toBeNull()
+        expect(tenant?.TenantId).not.toBeNull()
+        if (tenant?.TenantId) {
+          // Get other information
+          const existingService = await ds.getEcsService(tenant.TenantId, 'nginx').toPromise()
+          expect(existingService).not.toBeNull()
+          if (existingService) {
+            const existingTaskDefArn = new EcsTaskDefinitionArn(existingService.TaskDefinition)
+            const existingTaskDef = await ds
+              .getTaskDefinitionDetails(tenant.TenantId, existingService.TaskDefinition)
+              .toPromise()
+            const ImagePrev = existingTaskDef.ContainerDefinitions.find(cnt => cnt.Name === 'default')?.Image
+
+            // Build the request
+            const request = {
+              Name: 'nginx',
+              Image: ImagePrev === 'nginx:latest' ? 'nginx:1.21-alpine' : 'nginx:latest'
+            }
+
+            // Build the service updater
+            const updater = new EcsServiceUpdater(tenant, request, existingService, existingTaskDefArn, ds)
+
+            // Update the service
+            const done = await updater.buildServiceUpdate().toPromise()
+            expect(done.UpdateSucceeded).toBeTruthy()
+            expect(done.TaskDefinitionArn).not.toBeNull()
+
+            // Read it back and confirm that it changed.
+            const changedService = await ds.getEcsService(tenant.TenantId, 'nginx').toPromise()
+            expect(changedService).not.toBeNull()
+            if (changedService) {
+              expect(changedService?.TaskDefinition).toEqual(done?.TaskDefinitionArn)
+              const changedTaskDef = await ds
+                .getTaskDefinitionDetails(tenant.TenantId, changedService.TaskDefinition)
+                .toPromise()
+              const changedImage = changedTaskDef.ContainerDefinitions.find(cnt => cnt.Name === 'default')?.Image
+              expect(changedImage).toEqual(request.Image)
+            }
+          }
+        }
+      })
+    })
+  }
+})
---
+++ __tests__/duplocloud/server-updater.integration.test.ts
@@ -0,0 +1,52 @@
+import {expect, jest} from '@jest/globals'
+import {DataSource} from '../../src/duplocloud/datasource'
+import {DuploHttpClient} from '../../src/duplocloud/httpclient'
+import {ServiceUpdater} from '../../src/service-updater'
+
+describe('ServiceUpdater integration', () => {
+  // Integration tests.
+  const tenantId = process.env.duplo_tenant_id
+  if (!tenantId || !process.env.duplo_token) {
+    console.log('Skipping integration tests: duplo_token and/or duplo_tenant_id env var missing or empty')
+    it('is skipped', () => {})
+  } else {
+    describe('buildServiceUpdate', () => {
+      jest.setTimeout(15000)
+
+      it('patches a replication controller', async () => {
+        const ds = new DataSource(new DuploHttpClient())
+
+        // Get tenant
+        const tenant = await ds.getTenant(tenantId).toPromise()
+        expect(tenant).not.toBeNull()
+        expect(tenant?.TenantId).not.toBeNull()
+        if (tenant?.TenantId) {
+
+          // Get other information
+          const existing = await ds.getReplicationController(tenant.TenantId, 'website').toPromise()
+          expect(existing).not.toBeNull()
+          if (existing) {
+            const pods = await ds.getPodsByService(tenant.TenantId, 'website').toPromise()
+
+            // Build the request
+            const request = {
+              Name: 'website',
+              Image: existing?.Image == 'nginx:latest' ? 'nginx:1.21-alpine' : 'nginx:latest',
+              AgentPlatform: 0
+            }
+
+            // Build the service updater
+            const updater = new ServiceUpdater(tenant, request, existing, pods, ds)
+
+            // Update the service
+            const done = await updater.buildServiceUpdate().toPromise()
+
+            // Read it back and confirm that it changed.
+            const result = await ds.getReplicationController(tenant.TenantId, 'website').toPromise()
+            expect(result?.Image).toEqual(request.Image)
+          }
+        }
+      })
+    })
+  }
+})
